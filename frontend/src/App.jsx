62c31d5d66147effc32f4317aade7efd674f80c9
brandao-wesley <brandao.wesley.wb@gmail.com>
2025-10-28 18:10:07 -0300

    chore: add frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { AppointmentForm, AppointmentList } from './components';
 
 function App() {
   return (
     <div>
       <h1>Sistema de Agendamento</h1>
+      <AppointmentForm />
+      <AppointmentList />
     </div>
   );
 }
